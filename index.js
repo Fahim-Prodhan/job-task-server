62ec33d5140a7a606a82e23f1a02c4211c2c48cc
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-08-15 13:35:18 +0600

    get all products api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const express = require("express");
@@ -7,43 +7,52 @@ const app = express();
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djweinm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
 // middleware
 app.use(
     cors({
-      origin: ["http://localhost:5173"],
-      credentials: true,
+        origin: ["http://localhost:5173"],
+        credentials: true,
     })
-  );
-  app.use(express.json());
-  app.use(cookieParser());
+);
+app.use(express.json());
+app.use(cookieParser());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        const ProductCollections = client
+            .db("productDB")
+            .collection("products");
+
+        app.get('/api/products', async (req, res) => {
+            try {
+                const result = await ProductCollections.find().toArray()
+                res.send(result)
+            } catch (error) {
+                console.error('Error fetching products:', error);
+                res.status(500).json({ error: 'Failed to fetch products. Please try again later.' });
+            }
+        });
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Optionally, you can close the MongoDB client connection here
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 app.get("/", async (req, res) => {
     res.send("Testing Server");
-  });
-  app.listen(port, () => {
+});
+app.listen(port, () => {
     console.log("Backend is running at ", port);
-  });
+});
